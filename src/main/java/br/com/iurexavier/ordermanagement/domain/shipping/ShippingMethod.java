2c75c3e0b447842ffcfb2f0ec6fb21be7b507a17
iuxie <iurexavier10@gmail.com>
2025-12-22 13:39:03 -0300

    feat: add shipping result and strategy

--- src/main/java/br/com/iurexavier/ordermanagement/domain/shipping/ShippingMethod.java
+++ src/main/java/br/com/iurexavier/ordermanagement/domain/shipping/ShippingMethod.java
@@ -1,4 +1,7 @@
 package br.com.iurexavier.ordermanagement.domain.shipping;
 
+import br.com.iurexavier.ordermanagement.domain.order.Order;
+
 public interface ShippingMethod {
+    ShippingResult ship(Order order);
 }
--- src/main/java/br/com/iurexavier/ordermanagement/domain/shipping/ShippingResult.java
+++ src/main/java/br/com/iurexavier/ordermanagement/domain/shipping/ShippingResult.java
@@ -1,4 +1,26 @@
 package br.com.iurexavier.ordermanagement.domain.shipping;
 
 public class ShippingResult {
+    private final boolean success;
+    private final String message;
+
+    public ShippingResult(boolean success, String message) {
+        validateFields(message);
+        this.success = success;
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    private void validateFields(String message) {
+        if (message == null || message.isBlank()) {
+            throw new IllegalArgumentException("A mensagem é obrigatória!");
+        }
+    }
 }
